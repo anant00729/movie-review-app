d184e88e48c9e7daadc43fb721f7ddf1d1881d60
Anant <anant.awasthy@tatadigital.com>
2020-12-07 00:08:43 +0530

    asd

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 var bodyParser = require("body-parser");
 const db = require("./config/database");
 const cors = require("cors");
+const path = require("path");
 const userRouter = require("./v1/routes/user");
 const reviewRouter = require("./v1/routes/review");
 const movieRouter = require("./v1/routes/movie");
@@ -26,6 +27,9 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.use(express.static("public/build"));
+app.use(express.static("public"));
+
 app.use("/v1/auth", userRouter);
 app.use("/v1/review", reviewRouter);
 app.use("/v1/movie", movieRouter);
@@ -37,6 +41,10 @@ app.get("/test", (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "public", "build", "index.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on Port ${PORT}`);
