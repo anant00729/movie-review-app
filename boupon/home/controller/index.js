c9664b30ddda08b6bc9f999c2d1d32c2b97f10c3
Anant <anant.awasthy@tatadigital.com>
2020-12-31 15:27:17 +0530

    addded freedeals

--- boupon/home/controller/index.js
+++ boupon/home/controller/index.js
@@ -1,4 +1,10 @@
 const homeData = require("../dataSource/homeData.json");
+const freeDeals = require("../dataSource/freeDeals.json");
+
 exports.getHomeData = async (req, res) => {
   res.json(homeData);
+};
+
+exports.getFreeDeals = async (req, res) => {
+  res.json(freeDeals);
 };
--- boupon/home/routes/index.js
+++ boupon/home/routes/index.js
@@ -4,5 +4,6 @@ let _r = express.Router();
 
 // define the home page route
 _r.post("/getHomeData", getHomeData);
+_r.post("/freedeals", getHomeData);
 
 module.exports = _r;
