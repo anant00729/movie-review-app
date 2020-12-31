fd8deba9e4dbae68b5a1a216b87cfc3942ee55ff
Anant <anant.awasthy@tatadigital.com>
2020-12-31 15:36:54 +0530

    done

--- boupon/home/routes/index.js
+++ boupon/home/routes/index.js
@@ -1,9 +1,9 @@
 const express = require("express");
-const { getHomeData } = require("../controller");
+const { getHomeData, getFreeDeals } = require("../controller");
 let _r = express.Router();
 
 // define the home page route
 _r.post("/getHomeData", getHomeData);
-_r.post("/freedeals", getHomeData);
+_r.post("/freedeals", getFreeDeals);
 
 module.exports = _r;
