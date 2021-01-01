72e00fe12302be8eeb5a04115fda513d93bc736d
Anant <anant.awasthy@tatadigital.com>
2021-01-01 14:53:24 +0530

    added even and odd logic to getAllRecipies

--- boupon/home/controller/index.js
+++ boupon/home/controller/index.js
@@ -27,15 +27,37 @@ exports.getFreeDeals = async (req, res) => {
   res.json(freeDeals);
 };
 
+let isEven = true;
+
 exports.getAllRecipes = async (req, res) => {
+  let newHome = { ...homeRecipe };
+  let mainList = newHome.results;
+  // let mainList = [0, 1, 2, 3, 4, 5, 6];
+
   let results = [];
-  for (let i = 0; i < 10; i++) {
-    let ranIndex = getRandomInt(homeRecipe.results.length - 1);
-    let r = homeRecipe.results[ranIndex];
-    results.push(r);
+  if (isEven) {
+    for (let i = 0; i < mainList.length; i++) {
+      let r = {};
+      if (i % 2 === 0) {
+        let ranIndex = i % mainList.length;
+        r = mainList[ranIndex];
+        results.push(r);
+      }
+    }
+  } else {
+    for (let i = 0; i < mainList.length; i++) {
+      let r = {};
+      if (i % 2 === 1) {
+        let ranIndex = i % mainList.length;
+        r = mainList[ranIndex];
+        results.push(r);
+      }
+    }
   }
-  homeRecipe.results = results;
-  res.json(homeRecipe);
+
+  isEven = !isEven;
+  newHome.results = results;
+  res.json(newHome);
 };
 
 function getRandomInt(max) {
